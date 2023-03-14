df702cf3308d9ec3c7d646290d75197ab6ea625e
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-14 21:34:31 +0300

    commit tree d75a4a3ef453f4ed9107b72d1b91623e714c0572

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -20,12 +20,22 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
-  end
+
+    #PATCH /birds/:id
+    def update
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        bird.update(bird_params)
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :speciesm :likes)
   end
 
 end
